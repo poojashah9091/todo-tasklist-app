91a781a1ad3a668a9c90dbef007c269bf0139ebe
Pooja Shah <poojashah9091@gmail.com>
2023-03-24 22:58:46 -0400

    Integration of TaskDetails component on Header and TaskItem components and setting up handlers

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import TaskDetails from "../TaskDetails";
 import logo from "../../images/logo.png";
 import './style.scss';
 
 const Header = () => {
+
+    const [showTaskDetailsModel, setShowTaskDetailsModel] = useState(false);
+
+    const addTaskHandler = () =>{
+        setShowTaskDetailsModel(true);
+    }
+
+    const handleClose = () => {
+        setShowTaskDetailsModel(false);
+    };
+
+
     return (
         <div className="headerContainer">
             <div className="logoContainer">
                 <img src={logo} alt="logo" className="logo"/>
             </div>
             <div className="CTAContainer">
-                <button className="primaryCTA">Add a Task</button>
-                <button className="secondaryCTA">Complete All</button>
-                <button className="secondaryCTA">Delete All</button>
+                <button className="primaryCTA" onClick={addTaskHandler}>Add a Task</button>
             </div>
+            {showTaskDetailsModel && <TaskDetails 
+                                        open = {showTaskDetailsModel} 
+                                        handleClose = {handleClose} 
+                                        actionType = "Add"
+                                    />
+            }
         </div>
     )
 }
--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import "./style.scss";
 import { useDispatch } from "react-redux";
 import { deleteTask } from "../../data/taskListSlice";
+import TaskDetails from "../TaskDetails";
 
 const TaskItem = ({desc, id}) => {
 
     const dispatch = useDispatch();
+    const [showTaskDetailsModel, setShowTaskDetailsModel] = useState(false);
 
     const checkBoxClickHandler = (id) => {
     }
@@ -17,7 +19,12 @@ const TaskItem = ({desc, id}) => {
     }
 
     const editHandler = () => {
+        setShowTaskDetailsModel(true);
     }
+
+    const handleClose = () => {
+        setShowTaskDetailsModel(false);
+    };
 
     return (
         
@@ -33,7 +40,14 @@ const TaskItem = ({desc, id}) => {
                 <div className="iconsContainer" onClick={editHandler}><EditIcon/></div>
                 <div className="iconsContainer" onClick={deleteHandler}><DeleteOutlineIcon/></div>
             </div>
-            
+            {showTaskDetailsModel && <TaskDetails 
+                                        open = {showTaskDetailsModel} 
+                                        handleClose = {handleClose} 
+                                        desc = {desc} 
+                                        actionType = "Update"
+                                        id = {id}
+                                    />
+            }
 
         </div>
         

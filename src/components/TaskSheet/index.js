568c9ccfce4ef722f9e20fe9970c44b3c7e6b526
Pooja Shah <poojashah9091@gmail.com>
2023-03-26 23:00:38 -0400

    Showcasing messages for CTA action on toast messages

--- src/components/TaskSheet/index.js
+++ src/components/TaskSheet/index.js
@@ -4,39 +4,52 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { fetchAllTasks } from "../../data/taskListSlice";
 import TaskItem from "../TaskItem";
-import { Dialog } from "@mui/material";
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 import "./style.scss";
 
 const TaskSheet = () => {
 
     const dispatch = useDispatch();
     const [open, setOpen] = useState(false);
-    const [dialogMessage, setDialogMessage] = useState("");
+    const [dialogMessage, setDialogMessage] = useState({type: "success", msg: ""});
     const taskFetchStatus = useSelector(store => store.taskList.status.GET_ALL);
     const taskDeleteStatus = useSelector(store => store.taskList.status.DELETE); 
+    const taskUpdateStatus = useSelector(store => store.taskList.status.UPDATE);
+    const taskAddStatus = useSelector(store => store.taskList.status.ADD);
     const taskItems = useSelector(store => store.taskList.tasks);
     const taskFetchError = useSelector(store => store.taskList.error.GET_ALL);
 
     useEffect(()=>{
         dispatch(fetchAllTasks());
-    }, []);
+    }, [dispatch]);
+
+    const toastMessage = (status) =>{
+        if(status==="succeeded"){
+            setOpen(true);
+            setDialogMessage({type: "success", msg:"Action performed successfully"});
+        }
+        else if(status==="failed"){
+            setOpen(true);
+            setDialogMessage({type: "error", msg:"This action cannot be performed on newly added tasks"});
+        }
+    }
    
     useEffect(()=>{
-        if(taskDeleteStatus==="succeeded"){
-            setOpen(true);
-            setDialogMessage("Task deleted successfully..!!");
-        }
-        else if(taskDeleteStatus==="failed"){
-            setOpen(true);
-            setDialogMessage("Task deletion failed..!!");
-        }
+        toastMessage(taskDeleteStatus);
     },[taskDeleteStatus]);
+
+    useEffect(()=>{
+        toastMessage(taskUpdateStatus);
+    },[taskUpdateStatus]);
+
+    useEffect(()=>{
+        toastMessage(taskAddStatus);
+    },[taskAddStatus]);
 
     const handleClose = () => {
         setOpen(false);
-        setDialogMessage("");
+        setDialogMessage({type: "success", msg: ""});
     };
 
     // Early return in case of error condition
@@ -50,24 +63,19 @@ const TaskSheet = () => {
     
     return(
         <div className="tasksheetContainer">
-            <Dialog
-                open={open}
-                onClose={handleClose}
-            >
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {dialogMessage}
-                    </DialogContentText>
-                </DialogContent>
-            </Dialog>
+            <Snackbar anchorOrigin={{horizontal: 'right', vertical: 'top'}} sx={{ marginTop: 5}} open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert variant="filled" onClose={handleClose} severity={dialogMessage.type==="success" ? "success" : "error"} sx={{ width: '100%' }}>
+                    {dialogMessage.msg}
+                </Alert>
+            </Snackbar>
             {
                 taskFetchStatus==='loading' ? 
                     <div className="loader">
                         <CircularProgress/>
                     </div> :
                     taskFetchStatus==='succeeded' && taskItems.length>=0 ? 
-                        taskItems.map((task) => (
-                            <TaskItem key={task?.id} id={task?.id} desc={task?.todo} isCompleted={task?.completed}/>
+                        taskItems.map((task, index) => (
+                            <TaskItem key={index} id={task?.id} desc={task?.todo} isCompleted={task?.completed}/>
                         ))  :
                         <div>Task list is empty</div>
             }
--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={customTheme}>
     <Provider store={store}>
-      {/* <React.StrictMode> */}
+      <React.StrictMode>
         <App />
-      {/* </React.StrictMode> */}
+      </React.StrictMode>
     </Provider>
   </ThemeProvider>
 );
